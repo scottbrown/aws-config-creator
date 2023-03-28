c1fdd39292509820209a4ab416150e0351d8fd82
Scott Brown <mail@typicalrunt.me>
2023-03-28 16:26:03 -0600

    Fixing pagination bug

--- cmd/rootCmd.go
+++ cmd/rootCmd.go
@@ -108,6 +108,8 @@ func handleRoot(cmd *cobra.Command, args []string) error {
 		if orgOutput.NextToken == nil {
 			break
 		}
+
+    token = orgOutput.NextToken
 	}
 
 	// print out the SSO session configuration to file
